aa618db75cb589d11739c475a93c37a23f2b4b42
Muhammad Asif <muhammad.asif@thriftmetrics.com>
2025-12-26 15:05:21 +0500

    solution and projects initialized

---
+++ src/Web/Orion.API/Program.cs
@@ -0,0 +1,35 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Orion.API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // app.MapOpenApi();
+}
+
+app.UseSwagger(); // Add this line to enable Swagger middleware
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Orion.API v1");
+});
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
