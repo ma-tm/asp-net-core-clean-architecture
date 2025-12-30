944cc49ebb31950027102b22a0be7b233d4950f5
Muhammad Asif <muhammad.asif@thriftmetrics.com>
2025-12-30 22:32:27 +0500

    un-necessary namespaces removed.

--- src/Application/Orion.Application/AccountAppLayer/UseCases/UserUseCases/GetUserById/GetUserByIdResponse.cs
+++ src/Application/Orion.Application/AccountAppLayer/UseCases/UserUseCases/GetUserById/GetUserByIdResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Orion.Application.AccountAppLayer.UseCases.UserUseCases.GetUserById
+﻿namespace Orion.Application.AccountAppLayer.UseCases.UserUseCases.GetUserById
 {
     public class GetUserByIdResponse
     {
